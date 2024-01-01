7c381d2105c4c52968be97b37944610c19aa18ff
J <justintheemailaddress@gmail.com>
2024-01-01 14:09:00 -0600

    #1

---
+++ functions.js
@@ -0,0 +1,11 @@
+function multiples(multipleOf, max) {
+    let multiplesOfArray =[]
+    for (let i=1; i<=max; i++) {
+        if (i % multipleOf === 0) {
+        multiplesOfArray.push(i)
+        }
+    }
+    return multiplesOfArray
+}
+
+console.log(multiples(2, 10))
